dca967414867f65edf40a9eb6bfeb265c8ad1636
duck <godqhr2256@gmail.com>
2021-10-10 17:54:03 +0900

    절대 경로 설정 및 useState 이동

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,13 +1,12 @@
 import { HashRouter as Router, Route, Switch} from "react-router-dom";// as를 이용하여 HashRouter의 이름을 Router로 변경.
-import { useState } from 'react';
 import Auth from '../routes/Auth';
 import Home from '../routes/Home';
 
 
 
-const AppRouter = () =>{ //Router을 이미 정의했기 때문에 다른 이름으로 정의
-    
-    const [isLoggedIn, setisLoggedIn] = useState(false); // useState
+const AppRouter = ({isLoggedIn}) =>{ //Router을 이미 정의했기 때문에 다른 이름으로 정의
+    //상위 컴포넌트에서 받은 프롭스는 구조분해 할당으로 사용
+   
     return( // switch를 이용하면 여러가지 라우트 중 하나만 렌더링하게 해줌.
         <Router>
             <Switch>  
