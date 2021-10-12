229e9664e104551586768a20a1bef7d19f2eed91
duck <godqhr2256@gmail.com>
2021-10-12 21:12:25 +0900

    firebase 회원가입 기능 구현

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,9 +1,16 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
+import {auth} from "fbase";
 
+import {
+    getAuth,
+    signInWithEmailAndPassword,
+    createUserWithEmailAndPassword,
+    } from 'firebase/auth';
 
 const Auth = () =>{
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [newAccount, setnewAccount] = useState(true);
 
     const onChange = (event) => {
        const {
@@ -15,8 +22,19 @@ const Auth = () =>{
               setPassword(value);
           }
         }
-    const onSubmit = (event) =>{
+    const onSubmit = async (event) =>{ // 서버로 값을 요청해서 받는 시간이 걸리기 때문에 async를 사용
         event.preventDefault();
+        try{
+           let data; 
+           if(newAccount) {
+            const data = await createUserWithEmailAndPassword(auth, email, password);
+            } else {
+            const data = await signInWithEmailAndPassword(auth, email, password);
+            }
+            console.log(data);
+        }catch (error){
+            console.log(error);
+        }
     }
     return (
       
@@ -24,7 +42,7 @@ const Auth = () =>{
 <form onSubmit = {onSubmit}>
     <input name="email" type ="text" placeholder="Email" required value={email} onChange={onChange}/>
     <input name="password" type ="password" placeholder="Password" required value = {password} onChange={onChange}/>
-    <input type ="submit" value="login"/>
+    <input type ="submit" value={newAccount ? "create Account" : "Login"} /> 
 </form>
 <div>
     <button>Continue with Google</button>
