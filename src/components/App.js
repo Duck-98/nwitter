84a049b94007ea32252a42a0679958c999cfdd05
duck <godqhr2256@gmail.com>
2021-10-19 18:25:39 +0900

    login 구현 완료

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,28 @@
 import AppRouter from 'components/Router';
-import {useState} from "react";
+import {useState, useEffect} from "react";
 import { auth } from "fbase";
 
  
 
 function App() {
+  
+  const [init, setInit] = useState(false)
   const [isLoggedIn, setIsLoggedIn] = useState(auth.currentUser)
   
+  useEffect(() => {
+    auth.onAuthStateChanged((user) =>{
+      if(user){
+        setIsLoggedIn(true);
+      }else{
+        setIsLoggedIn(false)
+      }
+      setInit(true);
+    });
+  }, []);
+
   return (
     <>
-  <AppRouter isLoggedIn = {isLoggedIn} />
+  {init ? <AppRouter isLoggedIn = {isLoggedIn} /> : "Initialzizing.."}
   <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
 
    </>
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -11,6 +11,7 @@ const Auth = () =>{
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [newAccount, setnewAccount] = useState(true);
+    const [error, setError] = useState("");
 
     const onChange = (event) => {
        const {
@@ -25,6 +26,7 @@ const Auth = () =>{
     const onSubmit = async (event) =>{ // 서버로 값을 요청해서 받는 시간이 걸리기 때문에 async를 사용
         event.preventDefault();
         try{
+           const auth = getAuth(); 
            let data; 
            if(newAccount) {
             const data = await createUserWithEmailAndPassword(auth, email, password);
@@ -33,17 +35,24 @@ const Auth = () =>{
             }
             console.log(data);
         }catch (error){
-            console.log(error);
+            setError(error.message); // 같은 이메일계정으로 다시 만든다면 오류가 발생
         }
-    }
+    };
+ const toggleAccount = () =>{
+     setnewAccount((prev) => !prev);
+ }   
+
     return (
       
 <div>
 <form onSubmit = {onSubmit}>
     <input name="email" type ="text" placeholder="Email" required value={email} onChange={onChange}/>
     <input name="password" type ="password" placeholder="Password" required value = {password} onChange={onChange}/>
-    <input type ="submit" value={newAccount ? "create Account" : "Login"} /> 
+    <input type ="submit" value={newAccount ? "create Account" : "Login"} />
+    {error} 
 </form>
+<span onClick={toggleAccount}>{newAccount ? "Sign in" : "Create Account" } </span>  
+
 <div>
     <button>Continue with Google</button>
     <button>Continue with Github</button>
