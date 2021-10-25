ffd5000c760c88d373f6c603b8c270a90d55d555
duck <godqhr2256@gmail.com>
2021-10-25 18:36:18 +0900

    트윗 삭제기능 완료/ 트윗 수정기능 ~ing

--- src/components/Nweet.js
+++ src/components/Nweet.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React ,{useState}from "react";
+import { dbService } from 'fbase';
+import {doc,deleteDoc,updateDoc} from "firebase/firestore";
+
+
 
 const Nweet = ({nweetObj, isOwner}) =>{
-    const onDeleteClick = () => {
+    const [editing, setEditing] = useState(false);
+    // 이 코드는 수정 버튼을 클릭했을 때 입력란과 버튼이 뜨게하는 기준점임.
+    const [newNweet, setNewNweet] = useState(nweetObj)
+    // 수정할 때 입력란에 기존의 트윗을 나타내기 위한 state
+    const onDeleteClick = async () => {
         const ok = window.confirm("Are you sure you want to delete this tweet?");
+        const NweetTextRef = doc(dbService,"nweets", `${nweetObj.id}`); 
+        // 데이터베이스에서 트윗의 아이디를 찾기 위한 코드.
         if(ok){
-            //delete tweet
-        }
+           await deleteDoc(NweetTextRef); // 트윗 삭제기능
+        };
 
     }
     return (
@@ -13,7 +23,7 @@ const Nweet = ({nweetObj, isOwner}) =>{
         <h4>{nweetObj.nweet}</h4>
         {isOwner && (
             <>
-                <button>Delete tweet</button>
+                <button onClick={onDeleteClick}>Delete tweet</button>
                 <button>Edit Nweet</button>            
             </>
         )}
