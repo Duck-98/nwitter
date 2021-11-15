70364acc09acb3d87cb68562aa4ad01acea3f63a
duck <godqhr2256@gmail.com>
2021-11-16 00:05:41 +0900

    Updates

---
+++ precache-manifest.a4137f8638ab4191c48d51487130fbbc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34eb9ebc97e06de0a7aa36df40a447ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08a1c43653c91ae0be54",
+    "url": "/static/css/main.825fed3e.chunk.css"
+  },
+  {
+    "revision": "245b8ce5ef419ce32572",
+    "url": "/static/js/2.d55fd361.chunk.js"
+  },
+  {
+    "revision": "beedd4aa548457aec04236a3c132a394",
+    "url": "/static/js/2.d55fd361.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08a1c43653c91ae0be54",
+    "url": "/static/js/main.746e80da.chunk.js"
+  },
+  {
+    "revision": "bd00b011b89de116e745",
+    "url": "/static/js/runtime-main.85d7b3f1.js"
+  }
+]);
